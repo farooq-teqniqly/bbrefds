f989d9d36cff4b091fd3318d9ec2a533bd0bc4e7
farooq <farooq@teqniqly.com>
2023-03-04 13:36:28 -0800

    Upload CSV script.

---
+++ setup/uploadFiles.ps1
@@ -0,0 +1,57 @@
+<#
+.SYNOPSIS
+Uploads all CSV files in the specified folder to a folder in an Azure Data Lake using the Azure CLI.
+
+.PARAMETER sourceFolderPath
+The local path of the folder containing the CSV files to be uploaded.
+
+.PARAMETER dataLakeName
+The name of the Azure Data Lake storage account.
+
+.PARAMETER dataLakeFolder
+The name of the folder in the Azure Data Lake where the CSV files will be uploaded.
+
+.PARAMETER fileSystemName
+The name of the file system in the Azure Data Lake where the CSV files will be uploaded.
+
+.PARAMETER resourceGroupName
+The name of the resource group containing the Azure Data Lake storage account.
+
+.PARAMETER subscriptionId
+The ID of the Azure subscription to use for the operation.
+#>
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]$sourceFolderPath,
+
+    [Parameter(Mandatory = $true)]
+    [string]$dataLakeName,
+    
+    [Parameter(Mandatory = $true)]
+    [string]$dataLakeFolder,
+
+    [Parameter(Mandatory = $true)]
+    [string]$fileSystemName,
+    
+    [Parameter(Mandatory = $true)]
+    [string]$resourceGroupName,
+    
+    [Parameter(Mandatory = $true)]
+    [string]$subscriptionId
+)
+
+az account set --subscription $subscriptionId
+
+$dataLakeAccountKey = az storage account keys list `
+    --account-name $dataLakeName `
+    --resource-group $resourceGroupName `
+    --query "[?keyName=='key1'].value" `
+    -o tsv
+
+az storage fs directory upload `
+    -f $fileSystemName `
+    --account-name $dataLakeName `
+    --account-key $dataLakeAccountKey `
+    --source $sourceFolderPath `
+    --destination-path $dataLakeFolder `
+    --recursive
