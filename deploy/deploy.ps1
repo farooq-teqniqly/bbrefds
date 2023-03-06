8cf6dbf02d4c6f5e816fdf8352b61a8d886d4164
farooq <farooq@teqniqly.com>
2023-03-05 20:54:19 -0800

    Synapse ARM template.

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -2,9 +2,6 @@
 .SYNOPSIS
 Uploads all CSV files in the specified folder to a folder in an Azure Data Lake using the Azure CLI.
 
-.PARAMETER solutionName
-A unique prefix for Azure resources.
-
 .PARAMETER location
 The location the Azure resources will be created in.
 
@@ -13,25 +10,51 @@ The ID of the Azure subscription to use for the operation.
 #>
 Param (
     [Parameter(Mandatory = $true)]
-    [string]$solutionName,
-
-    [Parameter(Mandatory = $true)]
     [string]$location,
     
     [Parameter(Mandatory = $true)]
     [string]$subscriptionId
 )
 
-$baseName = "$solutionName"
-$deploymentName = "$baseName-deployment-$(Get-Random)"
-$resourceGroupName = "$baseName-rg"
+function Get-UniquePrefix([int]$length) {
+    $alphabet = "abcdefghijklmnopqrstuvwxyz"
+    $uniquePrefix = ""
 
+    for ($i = 0; $i -lt $length; $i++) {
+        $random = Get-Random -Minimum 0 -Maximum ($alphabet.Length - 1)
+        $randomLetter = $alphabet[$random]
+        $uniquePrefix += $randomLetter
+    }
+
+    return $uniquePrefix
+}
+
+$uniquePrefix = Get-UniquePrefix(6)
+
+$deploymentName = "$uniquePrefix-deployment-$(Get-Random)"
+$resourceGroupName = "synapsepoc-$uniquePrefix-rg"
+
+
+Write-Host "Your unique prefix for the Azure resource group and resources is " -NoNewline
+Write-Host $uniquePrefix -ForegroundColor Cyan
+
+Write-Host "Your resource group name is " -NoNewline 
+Write-Host $resourceGroupName -ForegroundColor Cyan
 
 az account set --subscription $subscriptionId
 az group create --name $resourceGroupName --location $location
 
-az deployment group create `
+$templateFileName = Join-Path -Path $PSScriptRoot -ChildPath "azuredeploy.json"
+
+$output = az deployment group create `
     --resource-group $resourceGroupName `
     --name $deploymentName `
-    --template-file azuredeploy.json `
-    --parameters solutionName=$solutionName
+    --template-file $templateFileName `
+    --parameters uniquePrefix=$uniquePrefix `
+| ConvertFrom-Json
+
+if (!$output) {
+    Write-Warning "Deployment failed. Resourse group will be deleted in the background."
+    az group delete --name $resourceGroupName --yes --no-wait
+    exit 1
+}
